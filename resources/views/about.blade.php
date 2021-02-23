1c1246074ed9c8ad71f66a6fea560828dd9d32c9
Sofie <sova@live.nl>
2021-02-23 20:49:18 +0100

    styling general pages

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -6,5 +6,22 @@
 
 
 @section('content')
-    <h2>About</h2>
+    <div class="page_section">
+        <div class="about_info flex_sb">
+            <div class="about_text">
+                <h2>About</h2>
+                <p>On here you can sort <bold>YOUR</bold> business cards.</p>
+
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta dolorem exercitationem explicabo iste nam omnis placeat, praesentium soluta vitae voluptatibus?</p>
+
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia, tenetur.</p>
+
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus aut cum dolore esse exercitationem in, laboriosam maiores placeat quam quisquam rem similique vero vitae voluptatum.</p>
+            </div>
+            <div class="about_img">
+                <img class="customer_img" src="{{ asset('../images/customer_social.png') }}" alt="">
+
+            </div>
+        </div>
+    </div>
 @endsection
--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -6,5 +6,44 @@
 
 
 @section('content')
-    <h2>Contact</h2>
+    <div class="page_section">
+        <div class="contact_info flex_sb fl_row-rev">
+            <div class="contact_text">
+                <h2>Contact</h2>
+                <div class="contact_form">
+                    <form action="">
+                        <p>Want help with your business card?</p>
+
+                        <div class="form_group">
+                            <input class="input_field contact_input" type="text" placeholder="Full Name">
+                            <span class="label">You're Name</span>
+                        </div>
+                        <div class="form_group">
+                            <input class="input_field contact_input" type="tel" placeholder="+31 6 12345678">
+                            <span class="label">You're Phone Number</span>
+                        </div>
+                        <div class="form_group">
+                            <input class="input_field contact_input" type="email" placeholder="example@mail.com">
+                            <span class="label">You're Email Address</span>
+                        </div>
+                        <div class="form_group">
+                            <input class="input_field contact_input" type="text" placeholder="subject">
+                            <span class="label">Subject of Message</span>
+                        </div>
+                        <div class="form_group">
+                            <textarea class="input_field contact_input" placeholder="type here..."></textarea>
+                            <span class="label">Message</span>
+                        </div>
+                        <div class="form_group">
+                            <button class="btn contact_btn" type="submit">Submit</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <div class="contact_img">
+                <img class="customer_img" src="{{ asset('../images/customers_index.png') }}" alt="">
+            </div>
+        </div>
+    </div>
 @endsection
+
