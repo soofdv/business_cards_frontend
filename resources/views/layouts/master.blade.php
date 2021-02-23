0544252f79a7c0be4ba28ee8b51d94a0e1ab6e77
Sofie <sova@live.nl>
2021-02-23 20:54:31 +0100

    Copy right edit

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -54,7 +54,7 @@
 
      <footer class="footer-bg">
          <div class="container">
-             <p class="footer_text">&copy; Laravel Business Cards site, to improve your business experience!</p>
+             <p class="footer_text">Sofie &copy; Laravel Business Cards site, to improve your business experience!</p>
          </div>
      </footer>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
